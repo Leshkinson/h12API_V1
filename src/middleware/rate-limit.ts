c6d43dd7788be51c60dcb2abc9fb03a017c54b51
Oleg Lopatko <vfY2minSRcF9ym7pT>
2023-04-20 17:37:24 +0300

    fix11

--- src/middleware/rate-limit.ts
+++ src/middleware/rate-limit.ts
@@ -15,7 +15,7 @@ export const rateLimitGuard = async (req: Request, res: Response, next: NextFunc
 
     if (myCache.has(`${key}`)) {
         const foo = myCache.get(`${key}`)
-        if(Number(foo) > 6) {
+        if(Number(foo) > 10) {
             res.sendStatus(429)
 
             return;
